7e5ce120f6261e37957eb92c0f66f39081f002d0
gaetancorin <gaetancorin@gmail.com>
2025-08-23 14:35:37 +0200

    add eth average for 3 and 5 minutes with airflow

--- postgres/help_postgres/postgres_tables.sql
+++ postgres/help_postgres/postgres_tables.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS btc_usd_avg_indicator;
+DROP TABLE IF EXISTS eth_usd_avg_indicator;
 DROP TABLE IF EXISTS btc_usd;
 DROP TABLE IF EXISTS eth_usd;
 
@@ -22,6 +23,14 @@ CREATE TABLE btc_usd_avg_indicator (
     datetime_utc TIMESTAMPTZ(0) NOT NULL UNIQUE
 );
 
+CREATE TABLE eth_usd_avg_indicator (
+    id SERIAL PRIMARY KEY,
+    eth_usd_id INT NOT NULL REFERENCES eth_usd(id) ON DELETE CASCADE,
+    price_avg_3m NUMERIC(15,4) NOT NULL,
+    price_avg_5m NUMERIC(15,4),
+    datetime_utc TIMESTAMPTZ(0) NOT NULL UNIQUE
+);
+
 INSERT INTO btc_usd (price, recorded_at)
 VALUES
     (29500.1234, '2025-08-12 15:30:00+00'),
@@ -38,3 +47,9 @@ VALUES
     (1, 116689.0100, 116605.4828, '2025-08-12 15:30:00+00'),
     (2, 116635.7280, 116576.8388, '2025-08-12 15:31:00+00'),
     (3, 116546.6313, 116572.0448, '2025-08-12 15:32:00+00');
+
+INSERT INTO btc_usd_avg_indicator (btc_usd_id, price_avg_3m, price_avg_5m, datetime_utc)
+VALUES
+    (1, 4689.0100, 4605.4828, '2025-08-12 15:30:00+00'),
+    (2, 4635.7280, 4576.8388, '2025-08-12 15:31:00+00'),
+    (3, 4546.6313, 4572.0448, '2025-08-12 15:32:00+00');
