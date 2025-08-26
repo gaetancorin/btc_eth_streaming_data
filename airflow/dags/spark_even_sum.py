ce9a3d3bba5d9dce7c0929c32cd815e15078798b
gaetancorin <gaetancorin@gmail.com>
2025-08-26 15:13:56 +0200

    spark connect to airflow directly when launch

--- airflow/dags/spark_even_sum.py
+++ airflow/dags/spark_even_sum.py
@@ -1,20 +1,45 @@
 from airflow.sdk import dag, task
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.apache.spark.hooks.spark_connect import SparkConnectHook
+from pyspark.sql import SparkSession
 from datetime import datetime
 
 @dag(
     start_date=datetime(2025, 1, 1),
     schedule=None,
-    catchup=False  # not recover old launch
+    catchup=False
 )
 def spark_even_sum():
 
-    spark_even_sum_task = SparkSubmitOperator(
-        task_id="spark_even_sum",
-        conn_id="spark_default",
-        application="/opt/airflow/dags/spark_files/even_sum.py",  # script PySpark
-        name="spark_even_sum",
-        verbose=True
-    )
+    @task
+    def run_even_sum():
+        # Récupérer la connexion Airflow
+        hook = SparkConnectHook(conn_id="my_spark")
+        conn = hook.get_connection(conn_id="my_spark")
+
+        # Afficher les infos pour debug
+        print(f"Host: {conn.host}")
+        print(f"Port: {conn.port}")
+        print(f"Extra: {conn.extra_dejson}")
+
+        # Construire l’URL Spark
+        master_url = f"spark://{conn.host}:{conn.port}"
+        deploy_mode = conn.extra_dejson.get("deploy-mode", "client")
+
+        # Créer une SparkSession
+        spark = SparkSession.builder \
+            .master(master_url) \
+            .appName("EvenSum") \
+            .config("spark.submit.deployMode", deploy_mode) \
+            .getOrCreate()
+
+        # Exemple simple (somme des pairs)
+        data = [(i,) for i in range(1, 1000001)]
+        df = spark.createDataFrame(data, ["number"])
+        even_sum = df.filter(df["number"] % 2 == 0).groupBy().sum("number")
+        even_sum.show()
+
+        spark.stop()
+
+    run_even_sum()
 
 spark_even_sum()
