230c1e92a2cac7da37eba4157a7ab929dd89a280
gaetancorin <gaetancorin@gmail.com>
2025-09-11 15:56:52 +0200

    add monitoring on ci cd pipeline

--- clean-orchestrator.ps1
+++ clean-orchestrator.ps1
@@ -2,20 +2,23 @@
 
 Write-Output "STOPPING AND REMOVING ALL CONTAINERS AND VOLUMES FOR PROJECT"
 
-# 1. Airflow
-docker-compose -p airflow_streaming -f airflow/docker-compose.yaml down -v
-
-# 2. Spark
-docker-compose -p spark_streaming -f spark/docker-compose.yaml down -v
-
-# 3. Mailhog
-docker-compose -p mailhog_streaming -f mailhog/docker-compose.yaml down -v
-
-# 4. Ingestion
+# Ingestion
 docker rm -f ingestion_streaming 2>$null
 docker image rm ingestion_streaming -f 2>$null
 
-# 5. Postgres (remove network too)
+# Airflow
+docker-compose -p airflow_streaming -f airflow/docker-compose.yaml down -v
+
+# Mailhog
+docker-compose -p mailhog_streaming -f mailhog/docker-compose.yaml down -v
+
+# Spark
+docker-compose -p spark_streaming -f spark/docker-compose.yaml down -v
+
+# Postgres (remove network too)
 docker-compose -p postgres_streaming -f postgres/docker-compose.yaml down -v
+
+# Monitoring (Python ->Prometheus->Grafana)
+docker-compose -p monitoring_streaming -f monitoring/docker-compose.yaml down -v
 
 Write-Output "DONE CLEANING ALL CONTAINERS AND VOLUMES"
--- orchestrator.ps1
+++ orchestrator.ps1
@@ -2,27 +2,32 @@
 
 Write-Output "GO CREATE ALL CONTAINERS FOR PROJECT !"
 
-# 1. Postgres -> create postgres_streaming_default network
+# Monitoring (Python ->Prometheus->Grafana)
+cd monitoring
+docker-compose -p monitoring_streaming up -d --build
+cd ..
+
+# Postgres -> create postgres_streaming_default network
 cd postgres
 docker-compose -p postgres_streaming up -d --build
 cd ..
 
-# 2. Spark
+# Spark
 cd spark
 docker-compose -p spark_streaming up -d --build
 cd ..
 
-# 3. Mailhog
+# Mailhog
 cd mailhog
 docker-compose -p mailhog_streaming up -d --build
 cd ..
 
-# 4. Airflow
+# Airflow
 cd airflow
 docker-compose -p airflow_streaming up -d --build
 cd ..
 
-# 5. Ingestion
+# Ingestion
 Write-Output "CREATE INGESTION CONTAINER"
 cd ingestion
 docker build -t ingestion_streaming .
